0dfd71431f38a964caa23d03bba280b69b9b1954
Aztek <aztekjaa@gmail.com>
2026-07-24 17:42:23 +0200

    Added final DM by bot to confirm status of application

--- cogs/apply.py
+++ cogs/apply.py
@@ -654,6 +654,14 @@ class ApplyCog(commands.Cog):
             screenshot_urls=application["screenshot_urls"],
         )
 
+        try:
+            await interaction.user.send(
+                f"✅ Your application for **{application['character_name']}** has been "
+                "posted for review. You'll get a DM once a reviewer makes a decision."
+            )
+        except discord.Forbidden:
+            pass  # DMs closed - the ephemeral confirmation below still covers this
+
         await interaction.followup.send(
             "Your application has been posted for review. You'll get a DM once "
             "a reviewer makes a decision.",
@@ -1036,15 +1044,14 @@ class ApplyCog(commands.Cog):
             title="How to Gear-check and get the permissions to sign up for raids",
             description=(
                 "Click the blue button below this message to start your gear-check process.\n\n"
-                "---"
-                ":one:. You'll be asked for your character's exact in-game name,\n"
-                ":two:. The bot will ask for your exact in-game name and fetch some data \n"
-                ":three:. You will receive a DM, the bot makes a suggestion on what your main spec is (this can be changed)\n"
-                ":four:. You are (optionally) given the ability to provide a note and/or screenshots - or skip this step\n"
-                ":five:. When you do not provide a note/screenshot and you do not press the 'skip' button - your application will be posted automatically after 3 minutes.\n"
-                ":six:. You have the option to provide more information at any time by typing `/update-application` to the bot's DM. \n"
-                ":seven:Your character will be reviewed by our recruitment-team and will be notified of the outcome\n\n"
 
+                "1. You'll be asked for your character's exact in-game name,\n"
+                "2. The bot will ask for your exact in-game name and fetch some data \n"
+                "3. You will receive a DM, the bot makes a suggestion on what your main spec is (this can be changed)\n"
+                "4. You are (optionally) given the ability to provide a note and/or screenshots\n"
+                "5. You have the option to provide more information at any time by just replying to the bot\n"
+                "\n\n"
+                "Your character will be reviewed by our recruitment-team and will be notified of the outcome\n"
                 "When approved, you will be granted the correct roles and further instructions on how to sign for the raid(s)"
             ),
             color=discord.Color.blurple(),
